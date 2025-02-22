566003950740759740e11a9421673ba79a461f04
Midhun Unni <midhununni457@gmail.com>
2025-02-22 21:09:02 +0530

    feat(client): add protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
 import { ThemeProvider } from "./components/theme-provider";
 import "./App.css";
 import { AuthProvider } from "./contexts/AuthContext";
+import DashBoard from "./pages/DashBoard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -12,6 +13,14 @@ function App() {
         <AuthProvider>
           <Routes>
             <Route path="/auth" element={<Auth />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <DashBoard />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </AuthProvider>
       </Router>
