847173720e6e84cd570aa491319f7f0d996efc83
Midhun Unni <midhununni457@gmail.com>
2025-02-22 14:05:10 +0530

    feat(client): add login/signup page using shadcn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Auth from './pages/Auth';
+import { ThemeProvider } from './components/theme-provider';
 import "./App.css";
 
 function App() {
   return (
-    <main className="container text-3xl font-bold">Hello World
-    </main>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
