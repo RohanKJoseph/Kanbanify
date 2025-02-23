7202c42d1083259bb7bbfcfd1776940edb85ba10
Midhun Unni <midhununni457@gmail.com>
2025-02-23 18:10:57 +0530

    feat(client): add invites icon to sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { SquareKanban, Bolt, LogOut } from "lucide-react";
+import { SquareKanban, Bolt, LogOut, Users } from "lucide-react";
 import Logo from "@/assets/logo.svg";
 import { useState, useEffect } from "react";
 import { useAuth } from "@/contexts/AuthContext";
@@ -13,7 +13,9 @@ const Sidebar = ({ onHoverChange }) => {
 
   return (
     <div
-      className={`pt-5 flex min-w-14 flex-col justify-start items-center h-screen bg-black border-r-2 border-zinc-400 transition-all duration-300 w-full ${isHovered && "min-w-40"}`}
+      className={`pt-5 flex min-w-14 flex-col justify-start items-center h-screen bg-black border-r-2 border-zinc-400 transition-all duration-300 w-full ${
+        isHovered && "min-w-40"
+      }`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -26,9 +28,13 @@ const Sidebar = ({ onHoverChange }) => {
         {isHovered && <p className="tracking-[-1px] -ml-4 -mb-1">KANBANIFY</p>}
       </div>
       <div className="flex flex-col items-center mt-10 space-y-10">
-        <a href="/" className="flex items-center gap-3 mb-[55vh]">
+        <a href="/" className="flex items-center gap-3">
           <SquareKanban size="35" />
           {isHovered && <p className="text-[18px]">Projects</p>}
+        </a>
+        <a href="/invites" className="flex items-center gap-3 mb-[55vh]">
+          <Users size="35" />
+          {isHovered && <p className="text-[18px]">Invites</p>}
         </a>
         <div className="flex items-center gap-3">
           <Bolt size="35" />
