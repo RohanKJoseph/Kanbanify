206e3d7809ea941493825449492e32e495784962
1337kid <45532566+1337kid@users.noreply.github.com>
2025-02-23 19:38:07 +0530

    feat(server): add swagger ui for api documentation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const authRoutes = require("./authRoutes");
 const projectRoutes = require("./projectRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/project", projectRoutes);
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
